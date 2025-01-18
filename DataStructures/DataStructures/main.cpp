16136087903b97c54070e169979dc3b8517eecb0
Anastasiia <mekhova.anastasiya@mail.ru>
2025-01-18 12:17:07 +0300

    added project files

---
+++ DataStructures/DataStructures/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStructures
+//
+//  Created by Анастасия Мехова on 18.01.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
