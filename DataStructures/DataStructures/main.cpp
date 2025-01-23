03e2b2ce9df831e07f5c7b1263a42d146ba4361b
Anastasiia <mekhova.anastasiya@mail.ru>
2025-01-23 12:15:31 +0300

    created TStack class

--- DataStructures/DataStructures/main.cpp
+++ DataStructures/DataStructures/main.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include "TVector.h"
+#include "TStack.h"
 
 int main(int argc, const char * argv[]) {
     TVector<int> t(4);
@@ -20,6 +21,23 @@ int main(int argc, const char * argv[]) {
     t.Remove(2);
     
     std::cout << t;
+    
+    
+//    TStack<int> t;
+//    t.Push(3);
+//    t.Push(5);
+//    std::cout << t.Top() << std::endl;
+//    t.Top() = 9;
+//    std::cout << t.Top() << std::endl;
+//    
+//    t.Pop();
+//    std::cout << t.Top() << std::endl;
+//    //t.Clear();
+//    
+//    
+//    TStack<int> f(t);
+//    std::cout << f.Top() << std::endl;
+//    
     
     return 0;
 }
